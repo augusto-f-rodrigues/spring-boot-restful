3e748f6e8854d2067f8cf6854e0c21f54a9c0ff7
Augusto Rodrigues <85464318+augusto-f-rodrigues@users.noreply.github.com>
2024-07-05 22:02:35 -0300

    feat: user model

---
+++ src/main/java/com/example/springboot/models/User.java
@@ -0,0 +1,27 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID id;
+
+    @Column(unique=true, nullable=false)
+    public String name;
+}
