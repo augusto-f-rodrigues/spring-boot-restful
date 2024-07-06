1d7abd34440a8de72ac70aaf755790389281217a
Augusto Rodrigues <85464318+augusto-f-rodrigues@users.noreply.github.com>
2024-07-05 22:02:23 -0300

    feat: added lombok

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,6 +1,10 @@
 package com.example.springboot.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
@@ -9,37 +13,20 @@ import java.util.UUID;
 
 @Entity()
 @Table(name = "TB_PRODUCTS")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public UUID productId;
-    @Column(columnDefinition = "text")
+
+    @Column(nullable = false, columnDefinition = "text")
     public String name;
+
+    @Column
     public BigDecimal value;
-
-    public UUID getProductId() {
-        return productId;
-    }
-
-    public void setProductId(UUID productId) {
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
 }
